6ba44ee4139631c8d91d36b6ddea363968f34b04
yoojin <yoojin9005@gmail.com>
2022-09-05 23:20:31 -0400

    commit tree 9a0f26d14c8cc4c8ff77a6530633b968b0a68b7b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+  private
+
+  def render_not_found(error)
+    render json: {message: [error.message]}, status: :not_found
+  end
+
+  def render_invalid(invalid)
+    render json: {message: [invalid.record.errors.full_message]}, status: :unprocessable_entity
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      items = find_user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    render json: find_item, include: :user
+  end
+
+  def create
+    item = find_user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
